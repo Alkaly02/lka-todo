e6c0d3a84cc824f1d779ab608c976e5b719d0fec
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2022-05-09 23:25:44 +0000

    adding the title to AllTask component

--- src/components/AllTask.js
+++ src/components/AllTask.js
@@ -11,20 +11,23 @@ const AllTask = ({todos, setTodos}) => {
 
     return (
         <div className="container mt-2">
+            <h1 className="text-center fs-1 mb-4">All tasks</h1>
             {
-               todos.map((todo, index) => (
-                <div className="card border-secondary mb-3" key={todo.id}>
-                    <h5 className="card-header d-flex">
-                        {todo.title}
-                        <button
-                            onClick={() => handleDelete(todo.id)} className="btn close-btn btn-secondary ms-auto fs-6"
-                        >x</button>
-                    </h5>
-                    <div className="card-body text-success">
-                        <p className="card-text text-secondary fs-6">{todo.body}</p>
+                (todos.length !== 0) ?  todos.map((todo, index) => (
+                    <div className="card border-secondary mb-3" key={todo.id}>
+                        <h5 className="card-header d-flex">
+                            {todo.title}
+                            <button
+                                onClick={() => handleDelete(todo.id)} className="btn close-btn btn-secondary ms-auto fs-6"
+                            >x</button>
+                        </h5>
+                        <div className="card-body text-success">
+                            <p className="card-text text-secondary fs-6">{todo.body}</p>
+                        </div>
                     </div>
-                </div>
-               ))
+                )) 
+                :    
+                <p>Oupss sorry... No todos !!!</p>
             }
         </div>
     )
