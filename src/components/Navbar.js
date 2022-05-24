fc0b1f77c70c2c97c7f48cefad3b975c884aabdc
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2022-05-24 21:06:25 +0000

    ajusting the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return ( 
-        <nav className="navbar mt-3 px-3 mb-5">
-            <h3>LKA Todo</h3>
-            <div className="links">
-                <Link to="completed" className="ms-2 p-2 btn-success">Completed task</Link>
-                <Link to="/lka-todo" className="m-2 p-2">All task</Link>
-                <Link to="create" className="ms-2 p-2">Add task</Link>
+        <nav className="navbar d-flex flex-column flex-sm-row justify-content-sm-between align-items-center mt-3 px-3 mb-5">
+            <h3 className="m-sm-0 mb-3 text-center">LKA Todo</h3>
+            <div className="links mb-3 m-sm-0">
+                <Link to="/completed" className="p-2 btn-success">Completed task</Link>
+                <Link to="/lka-todo" className="m-sm-2 mx-1 p-2">All task</Link>
+                <Link to="/create" className="ms-sm-2  p-2">Add task</Link>
             </div>
         </nav>
      );
